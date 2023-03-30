27e974c61408acb73ffbd539c1051f5a2e60f6d2
l9zyr <l9zyr95@163.com>
2023-03-30 17:57:32 +0800

    2023.03.30

---
+++ class/src/linkedList/test01/ListNode.java
@@ -0,0 +1,10 @@
+package linkedList.test01;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
--- class/src/linkedList/test01/Solution.java
+++ class/src/linkedList/test01/Solution.java
@@ -1,4 +1,45 @@
 package linkedList.test01;
 
+/**
+ *
+ * 题目地址：
+ * https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ *
+ * 题目：
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ *
+ * 解题思路：
+ *
+ */
+
+import java.util.LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
+    public int[] reversePrint(ListNode head) {
+
+        if (head == null) return new int[0];
+
+        LinkedList<Integer> ans = new LinkedList<>();
+
+        while (head != null) {
+            ans.add(head.val);
+            head = head.next;
+        }
+
+        // 反转，存储到数组中
+        int N = ans.size();
+        int[] result = new int[N];
+        for (int i = 0; i < N; i++) {
+            result[i] = ans.removeLast();
+        }
+        return result;
+    }
 }
