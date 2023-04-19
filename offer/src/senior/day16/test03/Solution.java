bd6a83f9620048eb0206cd1dadc87d3aaa3342ec
l9zyr <l9zyr95@163.com>
2023-04-19 23:23:38 +0800

    2023.04.18

--- offer/src/senior/day16/test03/Solution.java
+++ offer/src/senior/day16/test03/Solution.java
@@ -1,4 +1,25 @@
 package senior.day16.test03;
 
+/**
+ * 题目地址：
+ * https://leetcode.cn/problems/3Etpl5/solutions/?envType=study-plan-v2&id=coding-interviews-special
+ *
+ *
+ */
 public class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
 }
