113d099a92fcdb2a2e013df88dd1f3c76c1b2d40
l9zyr <l9zyr95@163.com>
2023-05-04 11:35:18 +0800

    2023.05.04

--- other/src/javaexercise/array/Demo001.java
+++ other/src/javaexercise/array/Demo001.java
@@ -10,9 +10,43 @@ import java.util.Arrays;
  */
 public class Demo001 {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5};
+        int[] arr = new int[]{5,2,1,4,3};
 
+        // 指定数组中的范围进行字典排序
+        //parallelSort 和 sort 用法类似，功能类似
+        Arrays.sort(arr, 1,4);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+
+        // 数组排序
+        // Arrays.sort
         Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+
+        // 赋值子数组
+        // Arrays.copyOfRange
+        int[] arrcopy = Arrays.copyOfRange(arr, 1, 3);
+        for (int i : arrcopy) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // 打印数组
+        // Arrays.toString()
+        System.out.println(Arrays.toString(arr));
+        // System.out.println(arr);  // 输出的是引用
+
+        //
+
+
+
 
     }
 }
--- other/src/javaexercise/collect/Demo001.java
+++ other/src/javaexercise/collect/Demo001.java
@@ -1,6 +1,11 @@
 package javaexercise.collect;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * Collections工具类的使用
@@ -8,6 +13,50 @@ package javaexercise.collect;
  */
 public class Demo001 {
     public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(4);
+        arrayList.add(2);
+        arrayList.add(5);
+        arrayList.add(3);
+        arrayList.add(1);
+        System.out.println(arrayList.toString());
+
+        // 交换元素
+        // Collections.swap
+        Collections.swap(arrayList, 0,1);
+        System.out.println(arrayList.toString());
+
+
+        // 随机打乱
+        // Collections.shuffle
+        Collections.shuffle(arrayList);
+        System.out.println(arrayList.toString());
+
+        // 最大值，最小值
+        System.out.println(Collections.max(arrayList));
+        System.out.println(Collections.min(arrayList));
+
+        // 排序
+        Collections.sort(arrayList);
+        System.out.println(arrayList.toString());
+
+        // 二分查找
+        // list 保证有序，返回目标值下标
+        System.out.println(Collections.binarySearch(arrayList, 3));
+
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(4);
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(3);
+        linkedList.add(1);
+        System.out.println(linkedList.toString());
+
+
+
+
 
     }
 }
